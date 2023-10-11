a31f8b260aafe7d26ddb13a6708bf8133e5d0e9b
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-10-11 18:52:13 -0300

    init project

---
+++ src/main/java/com/paybank/paybank/MsPaybankApplication.java
@@ -0,0 +1,13 @@
+package com.paybank.paybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPaybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPaybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paybank/paybank/MsPaybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paybank.paybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPaybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
